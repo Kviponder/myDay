779531cd3413ef9247581a18ee91332fbc74aea3
Kviponder <119442089+Kviponder@users.noreply.github.com>
2023-03-24 13:24:45 -0400

    Minor formatting changes

--- assets/script.js
+++ assets/script.js
@@ -2,35 +2,32 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  $(".saveBtn").on("click", function() {
+  $(".saveBtn").on("click", function () {
     const timeBlock = $(this).parent();
-    const hour = timeBlock.attr("id")
+    const hour = timeBlock.attr("id");
     const description = timeBlock.find(".description").val();
     localStorage.setItem(hour, description);
   });
 
   const currentHour = dayjs().hour();
 
-  $(".time-block").each(function() {
-   const timeBlock = $(this)
-   const hour = parseInt((timeBlock).attr("id").split("-")[1]);
-   
-   if (hour < currentHour) {
-    timeBlock.addClass("past");
-   } else if (hour === currentHour) {
-    timeBlock.addClass("present");
-   } else {
-    timeBlock.addClass("future");
-   }
-   
-  const savedDescription = localStorage.getItem(`hour-${hour}`);
-  if (savedDescription) {
-    timeBlock.find(".description").val(savedDescription);
-  }
-});
+  $(".time-block").each(function () {
+    const timeBlock = $(this);
+    const hour = parseInt(timeBlock.attr("id").split("-")[1]);
 
+    if (hour < currentHour) {
+      timeBlock.addClass("past");
+    } else if (hour === currentHour) {
+      timeBlock.addClass("present");
+    } else {
+      timeBlock.addClass("future");
+    }
 
-  
-$("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
+    const savedDescription = localStorage.getItem(`hour-${hour}`);
+    if (savedDescription) {
+      timeBlock.find(".description").val(savedDescription);
+    }
+  });
+
+  $("#currentDay").text(dayjs().format("dddd, MMMM D, YYYY"));
 });
- 
